12f39fce6fcee85d51998bb46ee17e50a7e3fa31
Julia Coll <julia.coll03@estudiant.upf.edu>
2022-05-02 20:14:49 +0200

    P2

--- P2_230583/Assets/RW/Scripts/HayMachine.cs
+++ P2_230583/Assets/RW/Scripts/HayMachine.cs
@@ -28,6 +28,7 @@ public class HayMachine : MonoBehaviour
     private void ShootHay()
     {
         Instantiate(hayBalePrefab, haySpawnpoint.position, Quaternion.identity);
+        SoundManager.Instance.PlayShootClip();
     }
 
     private void UpdateShooting()
--- P2_230583/Assets/RW/Scripts/Sheep.cs
+++ P2_230583/Assets/RW/Scripts/Sheep.cs
@@ -7,12 +7,17 @@ public class Sheep : MonoBehaviour
     public float runSpeed;
     public float gotHayDestroyDelay;
     private bool hitByHay;
+    private bool isDestroyed;
+
 
     public float dropDestroyDelay; 
     private Collider myCollider; 
     private Rigidbody myRigidbody;
 
     private SheepSpawner sheepSpawner;
+
+    public float heartOffset; 
+    public GameObject heartPrefab; 
 
 
     // Start is called before the first frame update
@@ -33,7 +38,17 @@ public class Sheep : MonoBehaviour
         sheepSpawner.RemoveSheepFromList(gameObject);
         hitByHay = true;
         runSpeed = 0; 
-        Destroy(gameObject, gotHayDestroyDelay); 
+        Destroy(gameObject, gotHayDestroyDelay);
+
+        Instantiate(heartPrefab, transform.position + new Vector3(0, heartOffset, 0), Quaternion.identity);
+
+        TweenScale tweenScale = gameObject.AddComponent<TweenScale>(); ; // Add a TweenScale component
+        tweenScale.targetScale = 0; 
+        tweenScale.timeToReachTarget = gotHayDestroyDelay; // TweenScale will take to the same time it takes to destroy the sheep
+
+        SoundManager.Instance.PlaySheepHitClip(); // Play sound
+
+        GameStateManager.Instance.SavedSheep();
     }
 
     private void OnTriggerEnter(Collider other) 
@@ -44,7 +59,7 @@ public class Sheep : MonoBehaviour
             Destroy(other.gameObject); //Destroy the hay bale.
             HitByHay();
         }
-        else if (other.CompareTag("DropSheep"))
+        else if (other.CompareTag("DropSheep") && !isDestroyed)
         {
             Drop();
         }
@@ -53,10 +68,17 @@ public class Sheep : MonoBehaviour
 
     private void Drop()
     {
+        // Debug.Log("drop");
+        GameStateManager.Instance.DroppedSheep();
+
         sheepSpawner.RemoveSheepFromList(gameObject);
         myRigidbody.isKinematic = false; // Make the sheep's rigidbody non-kinematic so it gets affected by gravity.
         myCollider.isTrigger = false; // Disable the trigger so the sheep becomes a solid object
-        Destroy(gameObject, dropDestroyDelay); // Destroy the sheep after the delay 
+        Destroy(gameObject, dropDestroyDelay); // Destroy the sheep after the delay
+        isDestroyed = true;
+
+        SoundManager.Instance.PlaySheepDroppedClip(); // Play sound
+
     }
 
     public void SetSpawner(SheepSpawner spawner)
